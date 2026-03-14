01b97fcec285a5c031508112d545a715bced5344
Adarsh malagond <adarshmalgond68@gmail.com>
2026-03-14 18:40:58 +0530

    404 maintenance page

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,7 @@
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
-
 export default function Home() {
   const router = useRouter();
-  useEffect(() => { router.replace('/login'); }, []);
+  useEffect(() => { router.replace('/maintenance'); }, []);
   return null;
 }
